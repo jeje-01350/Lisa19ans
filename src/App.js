c57b4710383ad8368250a83f88e8fa79bb552fe3
Jeremy <jeremy@sensei-france.fr>
2025-01-21 16:51:44 +0100

    feat(Print): add Print page with styled components and print-specific styles; include Google Fonts and new image asset

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { AnimatePresence } from 'framer-motion';
 import Home from './pages/Home';
 import Quiz from './pages/Quiz';
 import FinalMessage from './pages/FinalMessage';
+import Print from './pages/Print';
 import './App.css';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/quiz" element={<Quiz />} />
           <Route path="/message" element={<FinalMessage />} />
+          <Route path="/print" element={<Print />} />
         </Routes>
       </AnimatePresence>
     </Router>
